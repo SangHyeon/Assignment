a71d164e376bdfa6e26f3e76f6e4f909c6f10fe7
nasang <sktkddusrj@naver.com>
2015-05-13 22:18:26 +0900

    Not Fix Memory Error

--- SystemEngineering/Flask_test/hello.py
+++ SystemEngineering/Flask_test/hello.py
@@ -1,17 +1,18 @@
 from flask import Flask
 import os
 pipe_name = 'pipefile'
-if not os.path.exists(pipe_name):
-	os.mkfifo(pipe_name)
 
-pipeout = os.open(pipe_name, os.O_WRONLY)
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_workd():
+def hello_world():
 	os.write(pipeout, 'Hello World/n')
 	return 'Hello World!'
 
 if __name__ == '__main__' :
+	if not os.path.exists(pipe_name):
+		os.mkfifo(pipe_name)
+
+	pipeout = os.open(pipe_name, os.O_WRONLY)
 	app.run()
