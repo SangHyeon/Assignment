c5ed76661c4d727e61e49ba2a5916605e1d0d1b9
nasang <sktkddusrj@naver.com>
2015-05-21 22:59:14 +0900

    add annotation

--- SystemProgramming/Account/account.c
+++ SystemProgramming/Account/account.c
@@ -12,17 +12,19 @@ struct record {
 	int balance; //balance
 };
 
+//record LOCK & UNLOCK
 void rec_lock(int fd, int recno, int len, short type);
+
+//display account information
 void display(struct record *current);
 
 int main(int argc, char * argv[])
 {
 	struct record current;
-	int record_no;
-	int fd, pos, i, n;
-	char yse;
+	int record_no;//account number (0~99)
+	int fd, pos, n;
 	char operation;
-	int amount = 0;
+	int amount;
 
 	if( (fd = open("Account_File", O_RDWR)) == -1)
 		exit(1); //can not open file
@@ -30,71 +32,72 @@ int main(int argc, char * argv[])
 	while(1) {
 		printf("enter account number \n");
 		scanf("%d%*c", &record_no);
-		if(record_no <= 0 && record_no > 99)
+		if(record_no <= 0 && record_no >= 99) {
+			printf("Retry \n");
 			break;
-
+		}
 		printf("enter operation name \n");
 		scanf("%c%*c", &operation);
 
 		switch(operation) {
 			case 'r' : //inqury
-				rec_lock(fd, record_no, sizeof(struct record), F_RDLCK);
-				pos = record_no * sizeof(struct record);
-				lseek(fd, pos, SEEK_SET);
-				n = read(fd, &current, sizeof(struct record));
-				display(&current);
-				rec_lock(fd, record_no, sizeof(struct record), F_UNLCK);
+				rec_lock(fd, record_no, sizeof(struct record), F_RDLCK);//reader's lock
+				pos = record_no * sizeof(struct record);//position of current account info
+				lseek(fd, pos, SEEK_SET);//set position
+				n = read(fd, &current, sizeof(struct record));//read current account struct
+				display(&current);//display
+				rec_lock(fd, record_no, sizeof(struct record), F_UNLCK);//unlock
 				break;
 			case 'd' : //deposit
-				rec_lock(fd, record_no, sizeof(struct record), F_WRLCK);
+				rec_lock(fd, record_no, sizeof(struct record), F_WRLCK);//writer's lock
 				pos = record_no * sizeof(struct record);
 				lseek(fd, pos, SEEK_SET);
 				n = read(fd, &current, sizeof(struct record));
 				display(&current);
 				printf("enter amount\n");
 				scanf("%d%*c", &amount);
-				current.balance += amount;
+				current.balance += amount;//modify balance
 				lseek(fd, pos, SEEK_SET);
-				write(fd, &current, sizeof(struct record));
-				rec_lock(fd, record_no, sizeof(struct record), F_UNLCK);
+				write(fd, &current, sizeof(struct record));//write balance amount
+				rec_lock(fd, record_no, sizeof(struct record), F_UNLCK);//unlock
 				break;
-			case 'w' :
-				rec_lock(fd, record_no, sizeof(struct record), F_WRLCK);
+			case 'w' : //withdraw
+				rec_lock(fd, record_no, sizeof(struct record), F_WRLCK);//writer's lock
 				pos = record_no * sizeof(struct record);
 				lseek(fd, pos, SEEK_SET);
 				n = read(fd, &current, sizeof(struct record));
 				display(&current);
 				printf("enter amount\n");
 				scanf("%d%*c", &amount);
-				current.balance -= amount;
+				current.balance -= amount;//modify balance
 				lseek(fd, pos, SEEK_SET);
 				write(fd, &current, sizeof(struct record));
-				rec_lock(fd, record_no, sizeof(struct record), F_UNLCK);
+				rec_lock(fd, record_no, sizeof(struct record), F_UNLCK);//unlock
 				break;
 
-			case 'q' :
+			case 'q' : //quit
 				printf("EXIT PROGRAM\n ");
 				return 0;
-			default :
+			default : //error
 				printf("input error\n");
 				continue;
 		};
 	}
-	close(fd);
+	close(fd);//close file
 }
 
 void rec_lock(int fd, int recno, int len, short type)
 {
-	struct flock fl;
+	struct flock fl;//FILE LOCK Struct
 	switch (type) {
-		case F_RDLCK :
-		case F_WRLCK :
-		case F_UNLCK :
-			fl.l_type = type;
-			fl.l_whence = SEEK_SET;
-			fl.l_start = recno * len;
-			fl.l_len = len;
-			fcntl(fd, F_SETLKW, &fl);
+		case F_RDLCK ://reader's lock
+		case F_WRLCK ://writer's lock
+		case F_UNLCK ://unlock
+			fl.l_type = type;//type = F_RDLCK, F_WRLCK, F_UNLCK
+			fl.l_whence = SEEK_SET;//front of file
+			fl.l_start = recno * len;//lock start point
+			fl.l_len = len;//lock region length
+			fcntl(fd, F_SETLKW, &fl);//file, command, lock struct
 			return;
 		default:
 			return;
