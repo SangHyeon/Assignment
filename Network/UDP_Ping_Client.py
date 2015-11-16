bfa0ea17d4bc801ccb39d251f7db9a5f1945d491
nasang <sktkddusrj@naver.com>
2015-11-17 02:21:57 +0900

    modify create socket part location

--- Network/UDP_Ping_Client.py
+++ Network/UDP_Ping_Client.py
@@ -2,10 +2,11 @@ import random
 import time
 from socket import *
 
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.settimeout(1)#waiting one second
 for ping in range(10): # 0~9
-    clientSocket = socket(AF_INET, SOCK_DGRAM)
-    clientSocket.settimeout(1)#wait one second
     curtime =  time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+    #or using datetime.datetime.now() (import datetime)
     Ping = ping+1
     test = "Ping " + str(ping+1) + " " + curtime
     addr = ("127.0.0.1", 12000)#server address
@@ -14,7 +15,7 @@ for ping in range(10): # 0~9
     try:
         message, server = clientSocket.recvfrom(1024)#recieve upper test message from client
         RTT = time.time() - start
-        #print ping number and TEST(upper 'test')
+        #print ping number and upper test message
         print '#RECV : \"%s\"  #RTT : %f' % (message, RTT)
     except timeout:#if time out
         print 'Request timed out'
