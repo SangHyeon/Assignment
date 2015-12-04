feb770cfcf2f759ef973164b62e9590005a7a028
SangHyeon <sktkddusrj@naver.com>
2015-12-04 14:11:25 +0900

    can turn

--- OS/7.4.4 LineTracer_exam/main.c
+++ OS/7.4.4 LineTracer_exam/main.c
@@ -1,6 +1,5 @@
-
 /*********************************************************
-	적외선 센서를 이용한 라인트레이서 예제 프로그램
+    적외선 센서를 이용한 라인트레이서 예제 프로그램
 **********************************************************/
 
 #include <stdio.h>
@@ -14,98 +13,134 @@
 
 int main()
 {
-	int ret;
+    int ret;
+    int r_turn = 0;
+    int l_turn = 0;
 
-	unsigned char Line_Value,tmp_line=0xff;
+    unsigned char Line_Value,tmp_line=0xff;
 
-	ret = user_uart1_open("SAC1"); 	
-	
-	if (ret < 0)
-	{
-		printf("\n UART1 Open error!");
-		exit(0);
-	}	
+    ret = user_uart1_open("SAC1");
 
-	user_uart1_config(115200, 8, UART_PARNONE, 1);
+    if (ret < 0)
+    {
+        printf("\n UART1 Open error!");
+        exit(0);
+    }
 
-	init_keyboard();
-	 
-	printf("**************************\n");
-	printf(" RoboCAR Linetracer exam\n");
-	printf("**************************\n");
-	
-	RoboCAR_AllMotor_Control(STOP,0);
+    user_uart1_config(115200, 8, UART_PARNONE, 1);
 
-	while(1)
-	{
-		if(kbhit()) break;
-		
-		usleep(10*1000);
+    init_keyboard();
 
-		Line_Value= RoboCAR_Get_InfraredRay_Data();
-		
-		printf("InfraredRay = 0x%x\n",Line_Value);
-		
-		if(tmp_line == Line_Value) continue;
-		
-		tmp_line = Line_Value;
+    printf("**************************\n");
+    printf(" RoboCAR Linetracer exam\n");
+    printf("**************************\n");
 
-		switch(Line_Value){
-			
-			//전진
-			case 0xE7:	// 1110 0111
-			case 0xEF:	// 1110 1111
-			case 0xF7:	// 1111 0111
-				RoboCAR_AllMotor_Control(FORWARD,80);				
-			break;
-			
-			//좌회전
-			case 0xF3: //1111 0011
-			case 0xFB: //1111 1011 
-			case 0xF9: //1111 1001
-				RoboCAR_LeftMotor_Control(FORWARD,20);
-				RoboCAR_RightMotor_Control(FORWARD,80);
-			break;
-			
-			//우측으로 후진
-			case 0xFC: // 1111 1100
-			case 0xFD: // 1111 1101
-			case 0xFE: // 1111 1110
-				RoboCAR_LeftMotor_Control(BACKWARD,80);
-				RoboCAR_RightMotor_Control(BACKWARD,20);
-			break;
-			
-			//우회전
-			case 0xCF: // 1100 1111
-			case 0xDF: // 1101 1111
-			case 0x9F: // 1001 1111
-				RoboCAR_LeftMotor_Control(FORWARD,80);
-				RoboCAR_RightMotor_Control(FORWARD,20);
-			break;
-			
-			//좌측으로 후진	
-			case 0x3F: //0011 1111
-			case 0xBF: //1011 1111
-			case 0x7F: //0111 1111
-				RoboCAR_LeftMotor_Control(BACKWARD,20);
-				RoboCAR_RightMotor_Control(BACKWARD,80);		
-			break;
+    RoboCAR_AllMotor_Control(STOP,0);
 
-			// 정지
-			case 0xFF:			
-				RoboCAR_AllMotor_Control(STOP,0);
-			break;	
-		}// end switch
+    while(1)
+    {
+        if(kbhit()) break;
 
-					
-	}//end while		
-	
-	RoboCAR_AllMotor_Control(STOP,0);
-	
-	user_uart1_close();
-	close_keyboard() ;
+        usleep(10*1000);
 
-	return 0;
+        Line_Value= RoboCAR_Get_InfraredRay_Data();
+
+        printf("InfraredRay = 0x%x\n",Line_Value);
+
+        if(tmp_line == Line_Value) continue;
+
+        tmp_line = Line_Value;
+
+        switch(Line_Value){
+
+            //전진
+            case 0xE7:  // 1110 0111
+            case 0xEF:  // 1110 1111
+            case 0xF7:  // 1111 0111
+                RoboCAR_AllMotor_Control(FORWARD,50);
+            break;
+
+            //좌회전
+            case 0xF3: //1111 0011
+            case 0xFB: //1111 1011
+            case 0xF9: //1111 1001
+                RoboCAR_LeftMotor_Control(FORWARD,40);
+                RoboCAR_RightMotor_Control(FORWARD,60);
+            break;
+
+            //left turn angle
+            case 0xC0: // 1100 0000
+            case 0xE0: // 1110 0000
+            case 0xF0: // 1111 0000
+                //RoboCAR_LeftMotor_Control(BACKWARD, 70);
+                //RoboCAR_RightMotor_Control(FORWARD, 80);
+                RoboCAR_AllMotor_Control(FORWARD, 30);
+                l_turn = 1;
+                //RoboCAR_Move_Angle(3, 80, 90);
+                //usleep(300*1000);
+            break;
+
+            //우측으로 후진
+            case 0xFC: // 1111 1100
+            case 0xFD: // 1111 1101
+            case 0xFE: // 1111 1110
+                RoboCAR_LeftMotor_Control(BACKWARD,60);
+                RoboCAR_RightMotor_Control(BACKWARD,40);
+            break;
+
+            //우회전
+            case 0xCF: // 1100 1111
+            case 0xDF: // 1101 1111
+            case 0x9F: // 1001 1111
+                RoboCAR_LeftMotor_Control(FORWARD,60);
+                RoboCAR_RightMotor_Control(FORWARD,40);
+            break;
+
+            //right turn angle
+            case 0x03: // 0000 0011
+            case 0x07: // 0000 0111
+            case 0x0F: // 0000 1111
+                //RoboCAR_LeftMotor_Control(FORWARD, 80);
+                //RoboCAR_RightMotor_Control(BACKWARD, 70);
+                //RoboCAR_AllMotor_Control(FORWARD, 35);
+                //RoboCAR_Move_Angle(4, 80, 90);
+                //usleep(300*1000);
+                RoboCAR_AllMotor_Control(FORWARD, 30);
+                r_turn = 1;
+            break;
+
+            //좌측으로 후진
+            case 0x3F: //0011 1111
+            case 0xBF: //1011 1111
+            case 0x7F: //0111 1111
+                RoboCAR_LeftMotor_Control(BACKWARD,40);
+                RoboCAR_RightMotor_Control(BACKWARD,60);
+            break;
+
+            // 정지
+            case 0xFF:
+                if(l_turn) {
+                    RoboCAR_Move_Angle(3, 80, 85);
+                    usleep(2000*1000);
+                    l_turn = 0;
+                }
+                else if(r_turn) {
+                    RoboCAR_Move_Angle(4, 80, 85);
+                    usleep(2000*1000);
+                    r_turn = 0;
+                }
+                else
+                    RoboCAR_AllMotor_Control(STOP,0);
+            break;
+        }// end switch
+
+
+    }//end while
+
+    RoboCAR_AllMotor_Control(STOP,0);
+
+    user_uart1_close();
+    close_keyboard() ;
+
+    return 0;
 }
-
-
