c535b9e6a0d97088894f9b5b129733a204381c46
nasang <sktkddusrj@naver.com>
2015-12-05 00:33:49 +0900

    add robo direction

--- OS/test.c
+++ OS/test.c
@@ -8,10 +8,46 @@ int main() {
     int i;
     for(i=0;i<14;i++)
          printf("0 ~ %d : %d\n",i,d[i]);
-    printf("13 "); 
+    printf("13 ");
+    push(13);
     while(finish != 0) {
         printf("%d ", path[finish]);
+        push(path[finish]);
         finish = path[finish];
+    }
+    printf("\n");
+    while(top != -1) {
+        int start = pop();
+        int dest = TOP();
+        int temp = robo_state - dir[start][dest];
+        if(temp == 0) {
+            printf("F -");
+            //robo_state = F;
+        }
+        else if(temp == -2 || temp == 2) {
+            printf("B -");
+            robo_state -= -2;
+            if(robo_state < 0)
+                robo_state += 4;
+        }
+        else if(temp == 1 || temp == -3) {
+            printf("L -");
+            robo_state--;
+            if(robo_state == 0)
+                robo_state += 4;
+        }
+        else if(temp == -1 || temp == 3) {
+            printf("R -");
+            robo_state++;
+            if(robo_state == 5)
+                robo_state = 1;
+        }
+        else {
+            printf(" (ERROR : %d) -", temp);
+        }
+        //printf("%d - ", temp);
+        if(TOP() == 13)
+            break;
     }
     printf("\n");
     return 0;
--- OS/test.h
+++ OS/test.h
@@ -1,9 +1,19 @@
 #include<stdio.h>
 
-int map[14][14];
+#define F 1 //Foward
+#define R 2 //Right
+#define B 3 //Back
+#define L 4 //Left
+
+int map[14][14];//map
+int dir[14][14];//인접 노드의 방향
 int visited[14] = {0, };
 int path[14] = {0, };
 int d[14];
+int robo_state = F;//robo_car의 진행 방향
+
+int stack[15] ={0, };
+int top = -1;
 
 //막힌 경우 a와 b로 표시 -> 999로 설정
 //현재 위치는 x와 y로 표시
@@ -15,22 +25,43 @@ void init(int flag, int a, int b, int x, int y) {
             map[i][j] = 9999;
     }
     //map 초기화
-    map[0][1] = map[1][0] = 45;//w
-    map[1][2] = map[2][1] = 95;//l
-    map[2][3] = map[3][2] = 48;//r
-    map[2][4] = map[4][2] = 48;//l
-    map[3][5] = map[5][3] = 70;//l
-    map[4][6] = map[6][4] = 70;//r
-    map[5][7] = map[7][5] = 48;//5->7 : l , 7-> 5 : w
-    map[6][7] = map[7][6] = 48;//r
-    map[5][8] = map[8][5] = 48;//3-5-8:r, 7-5-8:w
-    map[7][9] = map[9][7] = 35;//
-    map[9][10] = map[10][9] = 22;//
-    map[10][11] = map[11][10] = 35;//
-    map[5][12] = map[12][5] = 70;//
-    map[8][13] = map[13][8] = 70;//
-    map[11][12] = map[12][11] = 70;//
-    map[12][13] = map[13][12] = 48;//
+    map[0][1] = map[1][0] = 45;
+    map[1][2] = map[2][1] = 95;
+    map[2][3] = map[3][2] = 48;
+    map[2][4] = map[4][2] = 48;
+    map[3][5] = map[5][3] = 70;
+    map[4][6] = map[6][4] = 70;
+    map[5][7] = map[7][5] = 48;
+    map[6][7] = map[7][6] = 48;
+    map[5][8] = map[8][5] = 48;
+    map[7][9] = map[9][7] = 35;
+    map[9][10] = map[10][9] = 22;
+    map[10][11] = map[11][10] = 35;
+    map[5][12] = map[12][5] = 70;
+    map[8][13] = map[13][8] = 70;
+    map[11][12] = map[12][11] = 70;
+    map[12][13] = map[13][12] = 48;
+
+    //해당 위치에서 다음 위치로 이동하는 방향
+    //robo_car 방향 - 가려는 방향 
+    // 0 : F // -2 or 2 : B // 1 or -3 : L // -1 or 3 : R
+    dir[0][1] = F; dir[1][0] = B;
+    dir[1][2] = L; dir[2][1] = R;
+    dir[2][3] = F; dir[3][2] = B;
+    dir[2][4] = B; dir[4][2] = F;
+    dir[3][5] = L; dir[5][3] = R;
+    dir[4][6] = L; dir[6][4] = R;
+    dir[5][7] = B; dir[7][5] = F;
+    dir[6][7] = F; dir[7][6] = B;
+    dir[5][8] = F; dir[8][5] = B;
+    dir[7][9] = L; dir[9][7] = R;
+    dir[9][10] = B; dir[10][9] = F;
+    dir[10][11] = L; dir[11][10] = R;
+    dir[5][12] = L; dir[12][5] = R;
+    dir[8][13] = L; dir[13][8] = R;
+    dir[11][12] = F; dir[12][11] = B;
+    dir[12][13] = F; dir[13][12] = B;
+
 
     if(flag) {
         map[a][b] = map [b][a] = 9999;
@@ -70,17 +101,15 @@ void dijkstra(int start) {
     }
 }
 
-/*int main() {
-    init(0, 0, 0, 0, 0);
-    dijkstra(0);
-    int finish = 13;
-    for(int i=0;i<14;i++)
-         printf("0 ~ %d : %d\n",i,d[i]);
-    printf("13 "); 
-    while(finish != 0) {
-        printf("%d ", path[finish]);
-        finish = path[finish];
-    }
-    printf("\n");
-    return 0;
-}*/
+//simple stack
+void push(int n) {
+    top++;
+    stack[top] = n;
+}
+int pop() {
+    top--;
+    return stack[top+1];
+}
+int TOP() {
+    return stack[top];
+}
