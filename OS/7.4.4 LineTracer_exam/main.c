0797752297633e9c501dd1450ba86b443f429939
nasang <sktkddusrj@naver.com>
2015-12-10 01:16:57 +0900

    test

--- OS/7.4.4 LineTracer_exam/main.c
+++ OS/7.4.4 LineTracer_exam/main.c
@@ -116,17 +116,17 @@ int main()
             case 0xF9: //1111 1001
             case 0xFC: // 1111 1100
             case 0xFE: // 1111 1110
-                //RoboCAR_LeftMotor_Control(FORWARD,40);
-                //RoboCAR_RightMotor_Control(FORWARD,90);
-                //usleep(200*1000);
-                RoboCAR_Move_Angle(4, 60, 5);
+                RoboCAR_LeftMotor_Control(FORWARD,30);
+                RoboCAR_RightMotor_Control(FORWARD,70);
+                usleep(200*1000);
+                /*RoboCAR_Move_Angle(4, 60, 5);
                 usleep(200*1000);
                 RoboCAR_AllMotor_Control(BACKWARD, 50);
                 usleep(100*1000);
                 RoboCAR_Move_Angle(3, 60, 5);
                 usleep(200*1000);
                 RoboCAR_AllMotor_Control(FORWARD, 50);
-                usleep(120*1000);
+                usleep(120*1000);*/
 
                 if(l_turn) {
                     //usleep(50*1000);
@@ -217,17 +217,17 @@ int main()
             case 0x9F: //1001 1111
             case 0x3F: //0011 1111
             case 0x7F: //0111 1111
-                /*RoboCAR_LeftMotor_Control(FORWARD,90);
-                RoboCAR_RightMotor_Control(FORWARD,40);
-                usleep(200*1000);*/
-                RoboCAR_Move_Angle(3, 60, 5);
+                RoboCAR_LeftMotor_Control(FORWARD,70);
+                RoboCAR_RightMotor_Control(FORWARD,30);
+                usleep(200*1000);
+                /*RoboCAR_Move_Angle(3, 60, 5);
                 usleep(200*1000);
                 RoboCAR_AllMotor_Control(BACKWARD, 50);
                 usleep(100*1000);
                 RoboCAR_Move_Angle(4, 60, 5);
                 usleep(200*1000);
                 RoboCAR_AllMotor_Control(FORWARD, 50);
-                usleep(120*1000);
+                usleep(120*1000);*/
                 if(l_turn) {
                     printf("InfraredRay = 0x%x\n",Line_Value);
                     //usleep(10*1000);
